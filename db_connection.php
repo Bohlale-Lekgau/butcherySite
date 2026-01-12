79f67cc6cbdc03f84ab110ba3801be45c8d64a54
Bohlale Lekgau <153212569+Bohlale-Lekgau@users.noreply.github.com>
2026-01-12 06:17:16 +0200

    Add files via upload

---
+++ db_connection.php
@@ -0,0 +1,13 @@
+<?php
+$host = 'localhost';
+$dbname = 'mphenama_butchery';
+$username = 'your_username';
+$password = 'your_password';
+
+try {
+    $pdo = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8", $username, $password);
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch(PDOException $e) {
+    die("Could not connect to the database $dbname :" . $e->getMessage());
+}
+?>
